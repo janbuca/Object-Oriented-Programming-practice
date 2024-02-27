f667685e7b76faa9bd0a29f7d0223565bb1e415c
janbuca <janbuca333@gmail.com>
2024-02-27 12:07:26 -0800

    Two class can be goning to by get and set Method

--- Java_Practice/src/Day_2/java_10.java
+++ Java_Practice/src/Day_2/java_10.java
@@ -1,5 +1,17 @@
 package Day_2;
-
+import java.util.Scanner;
 public class java_10 {
-    
+    public static void main(String[] firew){
+        Scanner input = new Scanner(System.in);
+
+        java_9 myJava_9 = new java_9() ;
+        System.out.printf("Initial name is : %s%n%n ", myJava_9.getName());
+        System.out.print("Please enter the name : ");
+        String theName = input.nextLine();
+        myJava_9.setName(theName);
+        System.out.println();
+        System.out.printf("Name in Object myjava_9 is : %s:%n%s%n", myJava_9.getName());
+        
+    }
+
 }
