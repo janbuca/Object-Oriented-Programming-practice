0c7048b4734c61f39907c1415871ba694c108db2
janbuca <janbuca333@gmail.com>
2024-02-27 12:24:44 -0800

    All of the Second example of get and set method

---
+++ Java_Practice/src/Day_2/java_13.java
@@ -0,0 +1,20 @@
+package Day_2;
+
+public class java_13 {
+    private String name; //instance variable name
+    public java_13(String name) //parameterized constructor
+    {  
+        this.name = name;
+    }//initializing the instance variable using parameter value
+
+    //Method to set the name
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+    
+   
+}
