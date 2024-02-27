62ac6fce48a8c7e0c108f9831e43843c252431ba
janbuca <janbuca333@gmail.com>
2024-02-27 09:26:55 -0800

    Some of note will be add

--- Java_Practice/src/Day_1/java_5.java
+++ Java_Practice/src/Day_1/java_5.java
@@ -1,5 +1,15 @@
 package Day_1;
-import java.util.Scanner;    
+import java.util.Scanner; 
+/*
+The range of values for an int is -2,147, 483,648 to 2,147, 483,648
+    In the Data Type int, float, double and char are called primitive type.
+        The size of a primitive data types is fixed in Java.
+        Primitive-type names are keywords and must appear in all lowercase letters.
+    
+
+*/
+
+   
 public class java_5 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);        
--- Java_Practice/src/Day_2/java_8.java
+++ Java_Practice/src/Day_2/java_8.java
@@ -1,4 +1,11 @@
 package Day_2;
+import java.util.Scanner;
+// Scanner enables a program to read data
+//     e.g (number and Strings) for use in a program.
 
 public class java_8 {
+    public static void main(String[] firew) {
+        Scanner input = new Scanner(System.in); 
+        
+    } 
 }
