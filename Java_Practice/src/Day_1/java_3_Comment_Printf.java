1686fae1cfa28d4d79ce6faaac4dd4a490487360
janbuca <janbuca333@gmail.com>
2024-02-26 17:56:39 -0800

    java

---
+++ Java_Practice/src/Day_1/java_3_Comment_Printf.java
@@ -0,0 +1,13 @@
+package Day_1;
+
+public class java_3_Comment_Printf {
+    public static void main(String[] args) {
+        // it is Single line comment 
+        
+        System.out.printf("Hello,    %s! I am %d years old.", "Tom", 20, "%n");
+        System.out.printf("%s%n%s%n", "Welcome", "Janbuca githu");
+        /* Multiline comment */
+    }// end method main
+
+        
+}// end class java_3
