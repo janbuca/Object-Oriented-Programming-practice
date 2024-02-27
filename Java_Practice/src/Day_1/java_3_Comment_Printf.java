10636750459db88be757db2a208c4c6e538d7da5
janbuca <janbuca333@gmail.com>
2024-02-26 18:00:26 -0800

    Java Star can be done

--- Java_Practice/src/Day_1/java_3_Comment_Printf.java
+++ Java_Practice/src/Day_1/java_3_Comment_Printf.java
@@ -4,8 +4,12 @@ public class java_3_Comment_Printf {
     public static void main(String[] args) {
         // it is Single line comment 
         
-        System.out.printf("Hello,    %s! I am %d years old.", "Tom", 20, "%n");
-        System.out.printf("%s%n%s%n", "Welcome", "Janbuca githu");
+        //System.out.printf("Hello,    %s! I am %d years old.", "Tom", 20, "%n");
+        System.out.println("***** **** *** ** *");
+        System.out.println("* ** *** **** *****");
+        System.out.println("***** **** *** ** *");
+        System.out.println("* ** *** **** *****");
+
         /* Multiline comment */
     }// end method main
 
