f31173072e0a0e1a375d8193f01fc31355ea5d26
janbuca <janbuca333@gmail.com>
2024-02-27 11:52:40 -0800

    Java Set and get Method can be Begin

---
+++ Java_Practice/src/Day_2/java_9.java
@@ -0,0 +1,13 @@
+package Day_2;
+
+public class java_9 {
+    private String name;
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+}
