6e69c87ee7139e00e1d28cbd6421381dac0ce4f5
janbuca <janbuca333@gmail.com>
2024-02-27 11:17:36 -0800

    Simple Arithmetic Operator Method can be done

--- Java_Practice/src/Day_1/java_6.java
+++ Java_Practice/src/Day_1/java_6.java
@@ -6,26 +6,25 @@ Arithmetic Operator
     in Java Programming Language.
 
 */
-
-
 public class java_6 {
     public static void main(String[] firew){
+
         System.out.println("Addition Ended:");
-               
-        
+        addition();
+
         System.out.println("Subtraction Ended:");
-        subtraction();        
-       
+        subtraction();
+
         System.out.println("Multiplication Ended:");
-        multiplication();        
-       
+        multiplication();
+
         System.out.println("Division Ended:");
-        division();        
-        
+        division();
+
         System.out.println("Remainder Ended:");
-        remainder();   
+        remainder();  
      }
-     public void addition(){
+     public static void addition(){
         Scanner input = new Scanner(System.in);
         int a, b, sum;
         System.out.print("Enter First Number : ");
@@ -33,9 +32,9 @@ public class java_6 {
         System.out.print("Enter Second Number : ");
         b=input.nextInt();
         sum=a+b;
-        System.out.println("Sum of "+a+" and "+b+" is : " +sum);  
+        System.out.println("Sum of "+a+" and "+b+" is : " +sum+"\n\n");  
      }
-     public void subtraction(){
+     public static void subtraction(){
         Scanner input = new Scanner(System.in);
         System.out.print("Enter the first number : ");
         int a, b, sub;
@@ -43,33 +42,37 @@ public class java_6 {
         System.out.print("Enter the Second Number : ");
         b=input.nextInt();
         sub=a-b;
-        System.out.println("The Subtraction" +a+"and"+b+"is :"+sub);
+        System.out.println("The Subtraction" +a+"and"+b+"is :"+sub+"\n\n");
      }
-     public void  multiplication(){
+     public static void  multiplication(){
         Scanner input = new Scanner(System.in);
-        System.out.print("\nEnter the first number : ");
+        System.out.print("Enter the first number : ");
         float m1, n1, mul;
         m1=input.nextFloat();
         System.out.print("Enter the second number : ");
         n1=input.nextFloat();
         mul=m1*n1;
-        System.out.println("The Multiplication of "+m1+" and "+n1+" is : "+mul);
+        System.out.println("The Multiplication of "+m1+" and "+n1+" is : "+mul+"\n\n");
      }
-     public void division(){
+     public static void division(){
         Scanner input = new Scanner (System.in);
+        System.out.print("Enter the division number : ");
         float d1=input.nextFloat();
+        System.out.print("Enter the second division : ");
         float d2=input.nextFloat();
         float div=d1/d2;
-        System.out.println("The Division of "+d1+" and "+d2+" is "+div);
+        System.out.println("The Division of "+d1+" and "+d2+" is "+div+"\n\n");
 
      }
-     public void remainder(){
+     public static void remainder(){
         Scanner input = new Scanner(System.in);
         double rem, r1, r2;
+        System.out.print("Enter th Remainder Number");
         r1 = input.nextDouble();
+        System.out.print("Enter th Remainder Number");
         r2 = input.nextDouble();
         rem = r1%r2;
-        System.out.println("The Remainder of "+r1+" and "+r2+" is "+rem);
+        System.out.println("The Remainder of "+r1+" and "+r2+" is "+rem+"\n\n");
      }
      
 }
