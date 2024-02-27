3d209a8f7d79f8bf324367763aae050d897421d1
janbuca <janbuca333@gmail.com>
2024-02-26 18:43:36 -0800

    Relational Operators can be practice

--- Java_Practice/src/Day_1/java_7.java
+++ Java_Practice/src/Day_1/java_7.java
@@ -1,7 +1,23 @@
 package Day_1;
 // Relational Operators
+
+import java.util.Scanner;
 public class java_7 {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+float num1, num2;
+
+       System.out.println("Enter the first Integer number");
+       num1 = input.nextInt(); 
+
+       System.out.println("Enter the Second Integer number");
+       num2 = input.nextInt(); 
+
+       System.out.println(num1 == num2);
+       System.out.println((num1 != num2));
+       System.out.println(num1 < num2);
+       System.out.println(num1 > num2);
+       System.out.println(num1 >= num2);
         
     }
 }
