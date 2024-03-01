8e2a9fe8f1ece5a5d4a0a08f988cd08c5ca85c3a
janbuca <janbuca333@gmail.com>
2024-03-01 11:32:02 -0800

    Java switch Multiple-Selection Statement, Break and Continue

---
+++ Java_Practice/src/Day_4/java_26.java
@@ -0,0 +1,45 @@
+package Day_4;
+
+import java.util.Scanner;
+
+public class java_26 {
+    public static void main(String[] firew) {
+        Scanner in = new Scanner(System.in);
+
+        // int c;
+        // System.out.println("Enter your choice");
+        // c = input.nextInt();
+        // switch (c) {
+        //     case 1:
+        //         System.out.println("Monday");
+        //         break;
+        //     case 2:
+        //         System.out.println("Tuesday");
+        //         break;
+        //     case 3:
+        //         System.out.println("Wednesday");
+        //         break;
+        //     case 4:
+        //         System.out.println("Thursday");
+        //         break;
+        //     case 5:
+        //         System.out.println("Friday");
+        //         break;
+        //     default:
+        //         System.out.println("Wrong Number!!!");
+
+
+        //         for(int i = 0; i<9; i++) {
+        //             if(i==5)
+        //             break;
+        //             System.out.println(i);
+        //             }
+        // }
+
+        for(int i = 0; i<9; i++) {
+            if(i==5)
+            continue;
+            System.out.println(i);
+            }
+    }
+}
