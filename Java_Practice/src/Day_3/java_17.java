7a059a60fdc8a7ba1d0c0c6ef312ae2971e824f1
janbuca <janbuca333@gmail.com>
2024-03-01 09:14:52 -0800

    It is java Control Structures Note and Example

---
+++ Java_Practice/src/Day_3/java_17.java
@@ -0,0 +1,21 @@
+package Day_3;
+import java.util.Scanner;
+
+public class java_17 {
+    public static void main(String[]fire){
+        Scanner in = new Scanner (System.in);
+
+        System.out.println("Enter the Student Mark ");
+        float mark = in.nextFloat();
+
+        if(mark >50){
+            System.out.println("He/She is deserves Grade A");
+
+        }else{
+            System.out.println("He/She is not Deserves Grade A");
+        }
+        
+
+    }
+    
+}
