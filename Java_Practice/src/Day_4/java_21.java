f2c6630233cf94fa0b573ef6146c1bbd1c365abd
janbuca <janbuca333@gmail.com>
2024-03-01 10:54:06 -0800

    java  Nested Loop example

--- Java_Practice/src/Day_4/java_21.java
+++ Java_Practice/src/Day_4/java_21.java
@@ -1,7 +1,32 @@
 package Day_4;
+
 import java.util.Scanner;
 
 public class java_21 {
-    public static void main(String[] firew){}
-    Scanner in = new Scanner(System.in);
+    public static void main(String[] firew) {
+        Scanner in = new Scanner(System.in);
+
+        int i = 0;
+        do {
+            System.out.println(i);
+            i++;
+        } while (i < 5);
+
+        int j = 5;
+        do {
+            System.out.println(j);
+            j++;
+        } while (j < 10);
+        System.out.println("this is the false " + j);
+
+
+        int n = 100, sum = 0;
+        do {
+            sum += n;
+            System.out.println(sum);
+            n++;
+        } while (n < 105);
+        System.out.println("this is the false " + n);
+    }
+
 }
---
+++ Java_Practice/src/Day_4/java_22.java
@@ -0,0 +1,25 @@
+package Day_4;
+
+import java.util.Scanner;
+
+public class java_22 {
+    public static void main(String[] firew) {
+
+        for (int i = 0; i < 2; i++)
+            for (int j = 0; j < 3; j++)
+                System.out.println(i);
+        System.out.println("\n***********************************************************************\n");
+
+        for (int i = 0; i < 2; i++)
+            for (int j = 0; j < 3; j++)
+                System.out.println(j);
+        System.out.println("\n***********************************************************************\n");
+
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 3; j++)
+                System.out.print(" " + j);
+            System.out.println();
+        }
+
+    }
+}
