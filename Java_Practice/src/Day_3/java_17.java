effc9a0b8f7e60341fe9993e108f5b93a794432d
janbuca <janbuca333@gmail.com>
2024-03-01 09:24:11 -0800

    It  Java Control Statement if....else Statements

--- Java_Practice/src/Day_3/java_17.java
+++ Java_Practice/src/Day_3/java_17.java
@@ -8,11 +8,28 @@ public class java_17 {
         System.out.println("Enter the Student Mark ");
         float mark = in.nextFloat();
 
-        if(mark >50){
-            System.out.println("He/She is deserves Grade A");
-
-        }else{
+        if(mark>=90){
+            System.out.println("He/She is deserves Grade A+");
+        }else if(mark>=85){
             System.out.println("He/She is not Deserves Grade A");
+        }else if(mark>=80){
+            System.out.println("He/She is not Deserves Grade A-");
+        }else if(mark>=75){
+            System.out.println("He/She is not Deserves Grade B+");
+        }else if(mark>=70){
+            System.out.println("He/She is not Deserves Grade B");
+        }else if(mark>=65){
+            System.out.println("He/She is not Deserves Grade B-");
+        }else if(mark>=60){
+            System.out.println("He/She is not Deserves Grade C+");
+        }else if(mark>=55){
+            System.out.println("He/She is not Deserves Grade C");
+        }else if(mark>=50){
+            System.out.println("He/She is not Deserves Grade C-");
+        }else if(mark>=45){
+            System.out.println("He/She is not Deserves Grade D");
+        }else if(mark<=45){
+            System.out.println("He/She is not Deserves Grade It is Fall");
         }
         
 
