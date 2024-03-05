eac89594b8a05b1ecee47129f051860086c7f9bc
janbuca <janbuca333@gmail.com>
2024-03-05 03:07:42 -0800

    All of the Constraction can be done

---
+++ Java_Practice/src/Day_5_SimpleProject/StudentTest.java
@@ -0,0 +1,12 @@
+package Day_5_SimpleProject;
+import java.util.Scanner;
+public class StudentTest {
+    public static void main (String[] argStrings){
+        Scanner in = new Scanner(System.in);
+        Student s1 = new Student("No Name 1", "No Id 1", "Not Determined", 0,  "Not Specified 1", "Not Started 1");
+        Student s2 = new Student("No Name 2", "No Id 2", "Not Determined", 0,  "Not Specified 2", "Not Started 2");
+        Student s3 = new Student("No Name 3", "No Id 3", "Not Determined", 0,  "Not Specified 3", "Not Started 3");
+        Student s4 = new Student("No Name 4", "No Id 4", "Not Determined", 0,  "Not Specified 4", "Not Started 4");
+        Student s5 = new Student("No Name 5", "No Id 5", "Not Determined", 0,  "Not Specified 5", "Not Started 5");
+    }
+}
