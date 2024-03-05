524bb88b91a9680903390d092a0f7a74af14af9a
janbuca <janbuca333@gmail.com>
2024-03-05 02:45:18 -0800

    In Day 5 Simple Project can be Begin

--- Java_Practice/src/Day_5/java_27.java
+++
@@ -1,8 +0,0 @@
-package Day_5;
-import java.util.Scanner;
-
-public class java_27 {
-    public static void main(String[] firew){
-
-    }
-}
---
+++ Java_Practice/src/Day_5_SimpleProject/Student.java
@@ -0,0 +1,20 @@
+package Day_5_SimpleProject;
+
+public class Student {
+    private String name;
+    private String id;
+    private String sex;
+    private int age;
+    private String dept;
+    private String year;
+
+    public Student (String n, String id, String sex, int age, String dept, String yr ){
+        name = n;
+        this.id = id;
+        this.sex = sex;
+        this.age = age;
+        this.dept = dept;
+        year = yr;
+    }
+    
+}
