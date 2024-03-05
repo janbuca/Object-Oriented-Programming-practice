77811c317d7d7cd285252f92fdf88fcdae4dc912
janbuca <janbuca333@gmail.com>
2024-03-05 03:25:09 -0800

    Set and Get Method can be setup or disgning the setup

--- Java_Practice/src/Day_5_SimpleProject/Student.java
+++ Java_Practice/src/Day_5_SimpleProject/Student.java
@@ -16,5 +16,42 @@ public class Student {
         this.dept = dept;
         year = yr;
     }
-    
+
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setId(String id){
+        this.id = id;
+    }
+    public void setSex(String sex){
+        this.sex = sex;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public void setDept(String dept){
+        this.dept =dept;
+    }
+    public void setYear(String yr){
+        year = yr;
+    }
+
+    public String getName(){
+        return name;
+    }
+    public String getId(){
+        return id;
+    }
+    public String getSex(){
+        return sex;
+    }
+    public int getAge(){
+        return age;
+    }
+    public String getDept(){
+        return dept;
+    }
+    public String getYear(){
+        return year;
+    }
 }
